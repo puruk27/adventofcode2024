d0e9a931f15c3cfaa7aad835bdc19bed7b5ff923
Purushottam kumar <purushottam1627@gmail.com>
2024-12-07 22:15:55 +0530

    Update Day1.py

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,20 +1,31 @@
-#part_1
 data = [line.strip() for line in open("day1_input.txt").readlines()]
 
-arr1 = sorted([int(d.split()[0]) for d in data])
-arr2 = sorted([int(d.split()[1]) for d in data])
+list1 = sorted([int(d.split()[0]) for d in data])
+list2 = sorted([int(d.split()[1]) for d in data])
 
-total = sum(abs(a - b) for a, b in zip(arr1, arr2))
-print(total)
+total = sum(abs(a - b) for a, b in zip(list1, list2))
+print(f"total distance: {total}")
 
 #part_2
+from collections import Counter
+
+
+def calculate_similarity_score(left_list, right_list):
+    # Count occurrences in the right list
+    right_count = Counter(right_list)
+
+    # Calculate similarity score
+    score = sum(num * right_count[num] for num in left_list)
+    return score
+
+
+# extract  left list and right list from input file
 data = [line.strip() for line in open("day1_input.txt").readlines()]
 
-arr1 = [int(d.split()[0]) for d in data]
-dict2 = {}
-for d in [int(d.split()[1]) for d in data]:
-    dict2[d] = dict2.get(d, 0) + 1
+left_list = sorted([int(d.split()[0]) for d in data])
+right_list = sorted([int(d.split()[1]) for d in data])
 
-sim = sum([n * dict2.get(n, 0) for n in arr1])
 
-print(sim)
+# Calculate and print the score
+similarity_score = calculate_similarity_score(left_list, right_list)
+print(f"Similarity Score: {similarity_score}")
